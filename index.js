cd8ab5ce34c510c4f147b78d77dfa7103ede1ba7
hunter-williams <dragynson@yahoo.com>
2022-08-04 09:30:34 -0700

    timers in sync - end on game over

--- index.js
+++ index.js
@@ -21,171 +21,166 @@ var questions = [
 
 var isGameOver = false;
 
-// global timer
-var quizTimer = 1;
+var quizTimer = 0;
 
-// if timer is done 
-if (quizTimer <= 0 || document.querySelector('.quizTimer').innerHTML === 'Done'){
-    console.log('end of quiz timer');
-    clearInterval(quizInterval);
-    gameOver();
-}  
-
-var currentQuestionIndex = 0
-if (currentQuestionIndex >= 3){
-    console.log('end of questions')
-    clearInterval(questionInterval);
-    gameOver();
-}
+var currentQuestionIndex = 0;
 var currentQuestion = questions[currentQuestionIndex];
+
 var correctAnswers = 0;
  
  
 
 function decreaseQuizTimer(quizInterval){
-    quizTimer--;
-    document.querySelector('.quizTimer').innerHTML = '';
-    document.querySelector('.quizTimer').innerHTML = quizTimer;
-   
-    if (quizTimer === 0){
-        document.querySelector('.quizTimer').innerHTML='Done';
+    if(isGameOver){
+        console.log("decreaseQuizTimer() game over");
         clearInterval(quizInterval);
-        console.log("decreaseQuizTimer() end of quiz");
         gameOver();
+    } else if (quizTimer <= 1){
+        // if timer is done
+        clearInterval(quizInterval);
+        isGameOver = true;
+    } else {
+        // timer running 
+        quizTimer--;
+        document.querySelector('.quizTimer').innerHTML = '';
+        document.querySelector('.quizTimer').innerHTML = quizTimer;
     }
 }
 
 function decreaseQuestionTimer(questionInterval){
-    questionTimer--;
-     document.querySelector('.questionTimer').innerHTML = '';
-     document.querySelector('.questionTimer').innerHTML = questionTimer;
-   
-     if (questionTimer <= 0 && currentQuestionIndex >= 2) {
-        // out of time no more questions
-
-        console.log("decreaseQuestionsTimer() out of time no mas q!");
-        quizTimer = 0;
+    if(isGameOver){
+        console.log("decreaseQuestionTimer() game over");
+        clearInterval(questionInterval);
         gameOver();
-    } else if (questionTimer <= 0 && currentQuestionIndex <= 2) {
-        // out of time show next question
-       
-        clearInterval(questionInterval); // stop timer
-        console.log("decreaseQuestionsTimer() out of time showing next question!");
+    } else if (questionTimer <= 0 && currentQuestionIndex >= 2) {
+        // out of time  AND  no more questions
+        clearInterval(questionInterval);
+        console.log("decreaseQuestionsTimer() out of time AND no more questions!",quizTimer, currentQuestionIndex);
+        quizTimer = 1;
+        isGameOver = true;
+    } else if (questionTimer <= 0 && currentQuestionIndex < 2) {
+        // out of time  AND  next question
+        clearInterval(questionInterval);
+        console.log("decreaseQuestionsTimer() out of time AND showing next question!", quizTimer, currentQuestionIndex);
 
-        // show next question - that starts another timer
         currentQuestionIndex ++;
-        displayQuestion(currentQuestionIndex);
+        displayQuestion(currentQuestionIndex);  // start timer
         quizTimer -= 10;
-    }if (questionTimer <= 0){
-        // out of time
-        clearInterval(questionInterval); // stop timer
-        console.log("decreaseQuestionsTimer() out of time  n!");
+
+    } else if (currentQuestionIndex >= 3){
+        // out of questions
+        console.log('decreaseQuestionsTimer() end of questions', currentQuestionIndex);
+        clearInterval(questionInterval);
+        isGameOver = true;
+    } else {
+        // timer running
+        questionTimer--;
+        document.querySelector('.questionTimer').innerHTML = '';
+        document.querySelector('.questionTimer').innerHTML = questionTimer;
     }
-    // else if (currentQuestionIndex >= 3){
-    //     //out of questions
-    //     console.log('displayQuestions() end of questions');
-    //     gameOver();
-    // } 
 }
 
 function quiz(){
+
     console.log("=== start ===")
     quizTimer = 60;
 
     document.querySelector('.score').innerHTML = 0;
     
     // start timer
-    decreaseQuizTimer(quizInterval); //skip first interval
+    decreaseQuizTimer(quizInterval); // skip first interval
     var quizInterval = setInterval(() => {decreaseQuizTimer(quizInterval)}, 1000);
     
     // first question
     displayQuestion(currentQuestionIndex);
+
+    // onclick local storage
+    document.querySelector(".saveScore").addEventListener('click', () => {
+        // console.log("name:", document.querySelector(".name").value);
+        // console.log("correct answers:", correctAnswers);
+        
+        window.localStorage.setItem( document.querySelector(".name").value, correctAnswers);
+        var get = localStorage.getItem(document.querySelector(".name").value);
+        document.querySelector(".leaderboard").innerHTML = ` ${document.querySelector(".name").value} - ${get} `;
+    })
     
-    if(isGameOver){
-        return
-    }
 }
 
 function displayQuestion(currentQuestionIndex){
-    if (quizTimer === 0){
-        console.log('displayQuestions() end of quiz timer');
+    // if time is out  OR  no more questions
+    if (quizTimer <= 0 || currentQuestionIndex >=3){
+        console.log('displayQuestions() end of quiz timer OR no more questions', quizTimer, currentQuestionIndex);
         clearInterval(questionInterval);
-        gameOver();
-    }
- 
-    // if (currentQuestionIndex >= 2){
-    //     console.log('displayQuestions() end of questions');
-    //     gameOver();
+        isGameOver = true;
+    } else {
+        console.log("=== display question ===");
         
-    // }
-    
-    console.log("=== display question ===");
-    questionTimer = 10;
-    
-    decreaseQuestionTimer(questionInterval); //skip first interval
-    var questionInterval = setInterval(() => {decreaseQuestionTimer(questionInterval)}, 1000);
+        // reset and start timer
+        questionTimer = 10;
+        decreaseQuestionTimer(questionInterval); // skip first interval
+        var questionInterval = setInterval(() => {decreaseQuestionTimer(questionInterval)}, 1000);
 
-    var currentQuestion = questions[currentQuestionIndex];
-    document.querySelector('.options').innerHTML = '';
-    document.querySelector('.question').innerHTML = '';
-    document.querySelector('.questionTimer').innerHTML = '';
-
- 
-
-    // question 
-    if (currentQuestionIndex <= 2 || currentQuestion.question){
-        console.log("current question")
+        // reset old options
+        document.querySelector('.options').innerHTML = '';
+        document.querySelector('.question').innerHTML = '';
+        document.querySelector('.questionTimer').innerHTML = '';
+        
+        // set new question
+        var currentQuestion = questions[currentQuestionIndex];
         document.querySelector('.question').textContent = currentQuestion.question;
-    } else{
-        console.log('displayQuestions() end of questions');
-        gameOver();
+ 
+        // create new options
+        currentQuestion.options.forEach( option => {
+            var optionButton = document.createElement("button");
+            optionButton.textContent = option.toUpperCase();
+            optionButton.setAttribute("value",option);
+
+            optionButton.addEventListener("click", () => {checkAnswer(optionButton.value)} );
+            document.querySelector('.options').appendChild(optionButton);
+        })
     }
-
-    // options
-     currentQuestion.options.forEach( option => {
-        var optionButton = document.createElement("button");
-        optionButton.textContent=option;
-        optionButton.setAttribute("value",option);
-
-        optionButton.addEventListener("click", () => {checkAnswer(optionButton.value)} );
-        document.querySelector('.options').appendChild(optionButton);
-    })
-
-
+ 
  
 }
 
-// call on click
+// call on option click
 function checkAnswer(clickedOption){
-
     var currentQuestion = questions[currentQuestionIndex];
 
-    // if true
-    if (clickedOption === currentQuestion.correctAnswer){
-        document.querySelector('.result').innerHTML = "true";
+    // current question
+    if (clickedOption === currentQuestion.correctAnswer ){
+        // if true  
+        document.querySelector('.result').innerHTML = "TRUE";
+        document.querySelector('.result').style.color = "green";
         correctAnswers++;
         console.log("true - correct answers: ", correctAnswers);
         document.querySelector('.score').innerHTML = '';
         document.querySelector('.score').innerHTML = correctAnswers;
 
-    }else if (clickedOption !== currentQuestion.correctAnswer ){
-        document.querySelector('.result').innerHTML = "false";
-        
-        if(quizTimer > 10){
-            quizTimer -= 10;
-        } else{
-            gameOver()
-        }
-
+    } else if (clickedOption !== currentQuestion.correctAnswer ){
+        // if false  
+        document.querySelector('.result').innerHTML = "FALSE";
+        document.querySelector('.result').style.color = "red";
         console.log("false - correct answers: ", correctAnswers);
-        console.log('out of time');
-        clearInterval(questionInterval);
-    }
+    } 
 
-    currentQuestionIndex++;
-    console.log("current index", currentQuestionIndex);
-    displayQuestion(currentQuestionIndex);
+    // next question
+    if (currentQuestionIndex < 2){
+        // more questions
+        currentQuestionIndex++;
+        console.log("checkanswer() more quesions  ", currentQuestionIndex);
+        displayQuestion(currentQuestionIndex);
+
+    } else if (currentQuestionIndex >= 2){
+         // no more quesions
+        console.log("checkanswer() no more questions", currentQuestionIndex);
+        isGameOver = true;
+    }
 }
+
+    
+
+
 
 function gameOver(){
     isGameOver = true;
@@ -194,27 +189,19 @@ function gameOver(){
 
     document.querySelector('.quizTimer').innerHTML='Done';
     document.querySelector('.questionTimer').innerHTML='Done';
-    // clearInterval(quizInterval);
-    // clearInterval(questionInterval);
 
-    // return
+    // remove quesitons
+    document.querySelector(".question").style.display = 'none';
+    document.querySelector(".options").style.display = 'none';
+    document.querySelector(".resultContainer").style.display = 'none';
+    
     // prompt local host
+    document.querySelector(".endGame").style.display = 'flex';
     document.querySelector(".saveScore").style.display = 'block';
     document.querySelector(".leaderboard").style.display = 'block';
     document.querySelector(".name").style.display = 'block';
     
-    document.querySelector(".saveScore").addEventListener('click', () => {
-        
-            console.log(document.querySelector(".name").value)
-            console.log(correctAnswers)
-            
-        // var saved = window.localStorage.setItem( document.querySelector(".name").value, correctAnswers)
-        var get = localStorage.getItem(document.querySelector(".name").value)
-        console.log(saved, get);
-    })
 
-    document.querySelector(".leaderboard").innerHTML = get;
-
-
+    
 }
 
